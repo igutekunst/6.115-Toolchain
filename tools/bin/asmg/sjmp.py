7b959698d5779cdd1889bdc8ecf0bc8acfadc820
Isaac Gutekunst <igutekunst@gmail.com>
2012-05-10 20:49:14 -0400

    merged in Isaac Evan's preprocessor

--- tools/bin/asmg/sjmp.py
+++
@@ -1,9 +0,0 @@
-import instruction
-
-class SJMP(instruction.Instruction):
-    opcode = 0x80
-    bytes = 2
-    description = 'SJMP jumps unconditionally to the address specified reladdr. Reladdr must be within -128 or +127 bytes of the instruction that follows the SJMP instruction.'
-    
-    def getHexa(self,args):
-        pass
